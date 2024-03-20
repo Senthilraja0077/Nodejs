83c3176a6da3cdbb1273cc606b5ce55738ef1663
Senthilraja0077 <164158394+Senthilraja0077@users.noreply.github.com>
2024-03-20 15:11:40 +0530

    Delete Node JS directory

--- Node JS/expressjs/Users/users.router.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const app=express();
-const users_data_controller=require('./users.controller');
-//app.use('/users',controller.getUsers());
-app.use(controller);
-app.listen(3000);
-
-
